eeb75b8095993dd75ff18e9ec6dbaa0e27161db4
Chérie Oduwole <bellapette0@gmail.com>
2021-12-18 01:30:25 +0000

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,15 +5,19 @@
  */
 
 $(document).ready(function(){
+  const loadTweets = () => {
+  $.ajax('/tweets', { method: 'GET', dataType: 'json', success: function(data) {
+    renderTweets(data);
+   } })
+   
+  };
+
+  //FORM SUBMISSION USING JQUERY/AJAX
   $("form").submit(function(event) {
     event.preventDefault();
     let data = $(this).serialize();
-    console.log(data);
     $.ajax('/tweets', { method: 'POST', data});
   })
-
-
-
 
   //CREATING DYNAMIC TWEETS
   const createTweetElement = (tweetData) => {
@@ -45,34 +49,7 @@ $(document).ready(function(){
       const $tweet = createTweetElement(tweetData);
       $('article.tweets-container').append($tweet);
     }
-  }
+  };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  
-  renderTweets(data);
-  
+  loadTweets();
 });
